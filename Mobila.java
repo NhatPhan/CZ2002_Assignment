3b42cc349dfb3caa7f39801571b724de115845d1
NhatPhan <minhnhat.cs.phan@gmail.com>
2015-11-12 23:01:20 +0800

    Create Mobila.java

---
+++ Mobila.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.io.Console;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Random;
+import java.text.ParseException;
+
+public class Mobila {
+	public static void main(String[] aArgs) throws IOException {
+		boolean cont = false;
+		while(true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("========= MOBILA ====================");
+			System.out.println("| 1. Admin Module                   |");
+			System.out.println("| 1. Movie-Goer Module              |");
+			System.out.println("| 3. Exit                           |");
+			System.out.println("=====================================");
+			int module = scan.nextInt();
+			switch(module) {
+				case 1:
+					AccountController accountCtrl = new AccountController("account.txt");
+					if(accountCtrl.authenticate()) {Admin.main(aArgs);};
+					break;
+				case 2:
+					MovieGoer.main(aArgs);
+					break;
+				case 3:
+					cont = false;
+					break;
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
