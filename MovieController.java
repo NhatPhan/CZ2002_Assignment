fe66a21d126cf49810a8182df9b32a81a6856326
Xuan Vu Dang <xuanvu001@e.ntu.edu.sg>
2015-11-12 18:54:28 +0800

    Update MovieController.java

--- MovieController.java
+++ MovieController.java
@@ -3,6 +3,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.List;
 import java.util.Arrays;
@@ -16,11 +17,16 @@ import java.util.Locale;
 import java.text.ParseException;
 
 public class MovieController {
-	private List<Movie> movieList = new ArrayList();
+	
+	/* file IO seperator */
 	public static final String SEPARATOR_IN1 = "#";
 	public static final String SEPARATOR_IN = ",";
 	public static final String SEPARATOR_IN_SHOWTIME = "|";
 	public static final String SEPARATOR_OUT_CLASS = "&";
+	
+	private List<Movie> movieList = new ArrayList(); // copy of data from storage for processing
+	
+	public static boolean admin = false; // access privilege
 		
 	public MovieController(String filename) {
 		// read String from text file
@@ -111,13 +117,26 @@ public class MovieController {
 	    }
 	    return data;
 	}
-		
-	// Write fixed content to the given file
+	/*	
+	// Append fixed content to the given file
 	public static void write(String fileName, List data) throws IOException  {
 		PrintWriter out = new PrintWriter(new FileWriter(fileName));
 		try {
 			for (int i = 0; i < data.size() ; i++) {
 				out.append((String)data.get(i) + '\n');
+			}
+	    }
+	    finally {
+	      out.close();
+	    }
+	}
+	*/
+	// Write fixed content to the given file
+	public static void write(String fileName, List data) throws IOException  {
+		PrintWriter out = new PrintWriter(new FileWriter(fileName));
+		try {
+			for (int i = 0; i < data.size() ; i++) {
+				out.println((String)data.get(i));
 			}
 	    }
 	    finally {
@@ -196,6 +215,12 @@ public class MovieController {
 		return showtime;
 	}
 	
+	
+	public void checkAndSelectSeats(Showtime showtime) {
+		CineplexController cineplexCtrl = new CineplexController("cineplex.txt");
+		cineplexCtrl.printList(showtime);
+	}
+	
 	public void printTopMovie() {
 		List<Double> ratingList = new ArrayList();
 		for(Movie movie : movieList) {
@@ -214,12 +239,284 @@ public class MovieController {
 					return;
 			}
 		}
-		
-	} 	
+	}
 	
-	public void checkAndSelectSeats(Showtime showtime) {
-		CineplexController cineplexCtrl = new CineplexController("cineplex.txt");
-		cineplexCtrl.printList(showtime);
+	// get & set access privilege
+	public static void setAccess ()
+	{
+		MovieController.admin = true;
+	}
+	public static boolean getAccess ()
+	{
+		return MovieController.admin;
+	}
+	
+	// create a new movie
+	public void addMovie ()
+	{
+		Scanner sc = new Scanner (System.in);
+		List<String> newMovie = toStringList(movieList);
+		
+		while (true)
+		{
+			StringBuilder st =  new StringBuilder();
+			System.out.print("\n======== NEW MOVIE ========");
+			System.out.print("\n* Title: ");
+			String title = sc.nextLine();
+			st.append(title);
+			st.append(SEPARATOR_IN1);
+			System.out.print("\n* Status: ");
+			String status = sc.nextLine();
+			st.append(status);
+			st.append(SEPARATOR_IN1);
+			System.out.print("\n* Synopsis: ");
+			String synopsis = sc.nextLine();
+			st.append(synopsis);
+			st.append(SEPARATOR_IN1);
+			System.out.print("\n* Director: ");
+			String director = sc.nextLine();
+			st.append(director);
+			st.append(SEPARATOR_IN1);
+			System.out.print("\n* Type: ");
+			String type = sc.nextLine();
+			st.append(type);
+			st.append(SEPARATOR_OUT_CLASS);
+			System.out.print("\n* Cast: ");
+			String cast = sc.nextLine();
+			while(!cast.equals("NONE")) {
+				st.append(cast);
+				st.append(SEPARATOR_IN);
+				cast = sc.nextLine();
+			}
+			st.deleteCharAt(st.length() - 1);
+			st.append(SEPARATOR_OUT_CLASS);
+			System.out.print("\n* showTime: ");
+			String movieShowTime;
+			boolean moreMovieShowTime = true;
+			while(moreMovieShowTime) {
+				System.out.print("\n* CineplexId : ");
+				String cineplexId = sc.next();
+				System.out.print("\n* CinemaId : ");
+				String cinemaId = sc.next();
+				System.out.print("\n* Day (00 to 31): ");
+				String day = sc.next();
+				System.out.print("\n* Month (Jan to Dec) : ");
+				String month = sc.next();
+				System.out.print("\n* Year : ");
+				String year = sc.next();
+				System.out.print("\n* Hour : ");
+				String hour = sc.next();
+				System.out.print("\n* Minute : ");
+				String minute = sc.next();
+				movieShowTime = cineplexId + "|" + cinemaId + "|" + month + " " + day + " " + hour + ":" + minute + ":00 " + year;
+				st.append(movieShowTime);
+				moreMovieShowTime = sc.next().equals("y");
+				if (moreMovieShowTime) {st.append(SEPARATOR_IN); }
+			}
+			st.append(SEPARATOR_OUT_CLASS);
+			System.out.print("\n* Rating and Written Review: ");
+			String rating;
+			String review;
+			boolean moreReview = true;
+			while(moreReview) {
+				System.out.print("\n*Rating: ");
+				rating = sc.next();
+				sc.nextLine();
+				System.out.print("\n*Review: ");
+				review = sc.nextLine();
+				st.append(rating);
+				st.append("::");
+				st.append(review);
+				System.out.print("\n* Add Movie Feedback (y or n): ");
+				moreReview = sc.nextLine().equals("y");
+				if(moreReview) {st.append(SEPARATOR_IN); }
+			}
+			newMovie.add(st.toString());
+			System.out.print("\n* Add Movie (y or n): ");
+			if (sc.nextLine().equals("n"))
+				break;
+		}
+		try {
+			write("movie.txt",newMovie);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sync();
+	}
+	
+	private void sync ()
+	{
+		MovieController updated = new MovieController ("movie.txt");
+		this.movieList = updated.getMovieList();
+	}
+	
+	public void updateMovie ()
+	{
+		Scanner sc = new Scanner (System.in);
+		String none = "NONE";
+		
+		while (true)
+		{
+			System.out.print("Enter EXACT Movie Title to be updated: ");	// search for movie to update
+			String title = sc.nextLine();
+			if (title.equals(none))
+				break;
+			int index = -1;
+			for (int i = 0; i < movieList.size(); i++)
+				if (movieList.get(i).getTitle().equals(title))
+				{
+					index = i;
+					break;
+				}
+			if (index == -1)	// search failed
+			{
+				System.out.println("No Movie Title existed in the database!!");
+				break;
+			}
+			Movie item = movieList.get(index);
+			System.out.print("Enter Updated Status: ");	// update STATUS
+			String status = sc.nextLine();
+			if (!status.equals(none))
+				item.setStatus(status);
+			System.out.print("Enter Updated Synopsis: ");	// update SYNOPSIS
+			String synopsis = sc.nextLine();
+			if (!synopsis.equals(none))
+				item.setSynopsis(synopsis);
+			System.out.print("Enter Updated Type: ");	// update TYPE
+			String type = sc.nextLine();
+			if (!type.equals(none))
+				item.setType(type);
+			System.out.println("Show Time Update:");	// update SHOWTIME
+			SimpleDateFormat sdf = new SimpleDateFormat("MMM dd HH:mm:ss yyyy",Locale.ROOT);
+			while (true)	// multiple instance of update
+			{
+				System.out.print("Enter Updated CineplexId: ");
+				String cineplexId = sc.next();
+				sc.nextLine();
+				if (cineplexId.equals("0"))
+					break;
+				System.out.print("Enter Updated CinemaId: ");
+				String cinemaId = sc.next();
+				sc.nextLine();
+				if (cinemaId.equals("0"))
+					break;
+				Calendar cal = Calendar.getInstance();	// build new Showtime
+				System.out.print("Enter Day (00 to 31): ");
+				String day = sc.nextLine();
+				System.out.print("Enter Month (Jan to Dec) : ");
+				String month = sc.nextLine();
+				System.out.print("Enter Year : ");
+				String year = sc.nextLine();
+				System.out.print("Enter Hour : ");
+				String hour = sc.nextLine();
+				System.out.print("Enter Minute : ");
+				String minute = sc.nextLine();
+				String date = month + " " + day + " " + hour + ":" + minute + ":00 " + year;
+				try {
+					cal.setTime(sdf.parse(date));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Showtime st = new Showtime(cineplexId, cinemaId, cal);
+				index = -1;
+				for (int i = 0; i < item.getShowTime().size(); i++)	// search for existing showtime in system
+					if (item.getShowTime().get(i).equals(st))
+					{
+						index = i;
+						break;
+					}
+				if (index == -1)	// if no match, create new Showtime for movie
+				{
+					List<Showtime> update = item.getShowTime();
+					update.add(st);
+					item.setShowTime(update);
+				}
+				else	// else exit update routine
+					break;
+			}
+		}
+		try {
+			write("movie.txt", toStringList(movieList));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sync();
+	}
+
+	public void deleteMovie () throws IOException
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		while (true)
+		{
+			System.out.println("Enter EXACT Movie Title to be deleted: ");
+			String title = sc.nextLine();
+			if (title.equals("NONE"))
+				break;
+			for (Movie movie : movieList)
+				if (movie.getTitle().equals(title))
+				{
+					movie.setStatus("End Of Showing");
+					write("movie.txt", toStringList(movieList));
+					sync();
+					return;
+				}
+			System.out.println("No Movie Title existed in the database!!");
+		}
+	}
+	
+	public static List<String> toStringList (List<Movie> movieList)
+	{
+		List<String> toFile = new ArrayList();
+		
+		for (Movie movie : movieList)
+		{
+			StringBuilder st =  new StringBuilder();
+			st.append(movie.getTitle());
+			st.append(SEPARATOR_IN1);
+			st.append(movie.getStatus());
+			st.append(SEPARATOR_IN1);
+			st.append(movie.getSynopsis());
+			st.append(SEPARATOR_IN1);
+			st.append(movie.getDirector());
+			st.append(SEPARATOR_IN1);
+			st.append(movie.getType());
+			st.append(SEPARATOR_OUT_CLASS);
+			for (String actor : movie.getCast())
+			{
+				st.append(actor);
+				st.append(SEPARATOR_IN);
+			}
+			st.deleteCharAt(st.length() - 1);
+			st.append(SEPARATOR_OUT_CLASS);
+			for (Showtime showtime : movie.getShowTime())
+			{
+				st.append(showtime.getCineplexId());
+				st.append(SEPARATOR_IN_SHOWTIME);
+				st.append(showtime.getCinemaId());
+				st.append(SEPARATOR_IN_SHOWTIME);
+				String showtimeStrLong = showtime.getCalendar().getTime().toString();
+				String showtimeStr = showtimeStrLong.substring(4,19) + showtimeStrLong.substring(23,28);
+				st.append(showtimeStr);
+				st.append(SEPARATOR_IN);
+			}
+			st.deleteCharAt(st.length() - 1);
+			st.append(SEPARATOR_OUT_CLASS);
+			for (Review review : movie.getReviewList())
+			{
+				st.append(review.getRating());
+				st.append("::");
+				st.append(review.getReview());
+				st.append(SEPARATOR_IN);
+			}
+			st.deleteCharAt(st.length() - 1);
+			toFile.add(st.toString());
+		}
+		
+		return toFile;
 	}
 	
 	public static void main(String[] aArgs) throws IOException {
@@ -307,12 +604,24 @@ public class MovieController {
 				moreReview = scan.nextLine().equals("y");
 				if(moreReview) {st.append(SEPARATOR_IN); }
 			}
-			alw.add(st.toString());
+			alw.add(st.toStringList());
 			System.out.print("\n* Add Movie (y or n): ");
 			moreMovie = scan.nextLine().equals("y");
 		}
 		write("movie.txt",alw);	
 		*/
+		MovieController mc = new MovieController("movie.txt");
+		for (Movie movie : mc.getMovieList())
+			mc.printDetails(movie);
+		mc.addMovie();
+		for (Movie movie : mc.getMovieList())
+			mc.printDetails(movie);
+		mc.updateMovie();
+		for (Movie movie : mc.getMovieList())
+			mc.printDetails(movie);
+		mc.deleteMovie();
+		for (Movie movie : mc.getMovieList())
+			mc.printDetails(movie);
 		
 	}	
 }
